9ebf8523a902708aa0c5b27888b08ca34899f708
Jan-Pascal van Best <janpascal@vanbest.org>
2013-06-26 21:54:43 +0200

    Release 1.3.0

--- project/Build.scala
+++ project/Build.scala
@@ -5,7 +5,7 @@ import play.Project._
 object ApplicationBuild extends Build {
 
   val appName         = "tweet-play2"
-  val appVersion      = "1.2.0"
+  val appVersion      = "1.3.0"
 
   val appDependencies = Seq(
     // Add your project dependencies here,
