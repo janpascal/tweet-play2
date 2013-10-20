be3cfc6bf428e72c2204e4db76915a9bb4d580c8
Jan-Pascal van Best <janpascal@vanbest.org>
2013-10-20 16:46:56 +0200

    Implement check for rate limiting, in case it hits, wait

--- app/tweet/APIFetcher.java
+++ app/tweet/APIFetcher.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.lang.Thread;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URLEncoder;
@@ -23,6 +24,7 @@ import org.apache.http.impl.client.DefaultHttpClient;
 //import org.json.simple.parser.JSONParser;
 //import org.json.simple.parser.ParseException;
 //
+import twitter4j.RateLimitStatus;
 import twitter4j.Query;
 import twitter4j.QueryResult;
 import twitter4j.Status;
@@ -81,27 +83,48 @@ public class APIFetcher {
           TwitterFactory tf = new TwitterFactory(twitterConfiguration);
           Twitter twitter = tf.getInstance();
 
-        long maxId=-1;
-		long previousLastId=Long.MAX_VALUE;
-		for( int k=0; k<maxPages; k++ ) {
-	        //query.setUntil("2013-06-06");
-            Query query = new Query(terms).count(pageSize).resultType("recent");
-			if(maxId>0) {
+          RateLimitStatus rateStatus = twitter.getRateLimitStatus("search").get("/search/tweets");
+          int remaining = rateStatus.getRemaining();
+
+          long maxId=-1;
+   	  long previousLastId=Long.MAX_VALUE;
+	  for( int k=0; k<maxPages; k++ ) {
+             //query.setUntil("2013-06-06");
+             while (remaining<10) {
+                 rateStatus = twitter.getRateLimitStatus("search").get("/search/tweets");
+                 log("RateLimit limit: "+rateStatus.getLimit());
+                 log("RateLimit remaining: "+rateStatus.getRemaining());
+                 log("RateLimit seconds to reset: "+rateStatus.getSecondsUntilReset());
+                 long secondsToWait = rateStatus.getSecondsUntilReset();
+                 log("Running into Twitter rate limiting, need to wait "+secondsToWait+" seconds...");
+                 if(secondsToWait>60) secondsToWait=59;
+                 try {
+                     Thread.sleep((1+secondsToWait)*1000);
+                 } catch (InterruptedException e) {
+                     log("Wait interrupted "+e.toString());
+                     break;
+                 }
+                 rateStatus = twitter.getRateLimitStatus("search").get("/search/tweets");
+                 remaining = rateStatus.getRemaining();
+             }
+             remaining--;
+             Query query = new Query(terms).count(pageSize).resultType("recent");
+	     if(maxId>0) {
 			    query.setMaxId(maxId);
 			}
-            log("Query: "+query.toString());
-	        QueryResult result = twitter.search(query);
-	        log("Aantal resultaten: " +result.getTweets().size());
+             log("Query: "+query.toString());
+	     QueryResult result = twitter.search(query);
+	     log("Aantal resultaten: " +result.getTweets().size());
 
-	        long lastId=Long.MAX_VALUE;
-            for (Status status : result.getTweets()) {
+	     long lastId=Long.MAX_VALUE;
+             for (Status status : result.getTweets()) {
                 Long id = status.getId();
                 if (id<lastId) lastId=id;
                 handleTweet(queryName, status);
                 //System.out.println(id+"@" + status.getUser().getScreenName() + ":" + status.getCreatedAt() +":"+ status.getText());
-            }
-  	      	System.err.println("last id: "+lastId);
-            maxId = lastId;
+             }
+  	     System.err.println("last id: "+lastId);
+             maxId = lastId;
 			try {
 				Thread.sleep(100);
 			} catch (InterruptedException e1) {
