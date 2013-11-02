da1ab86dce3c63c11716e2f495d381ba009d37ce
Jan-Pascal van Best <janpascal@vanbest.org>
2013-11-02 20:43:46 +0100

    Convert database character set to 4-byte utf8

---
+++ conf/evolutions/default/9.sql
@@ -0,0 +1,12 @@
+# --- !Ups
+
+alter table tweet convert to character set utf8mb4;
+alter table job convert to character set utf8mb4;
+alter table stream_config convert to character set utf8mb4;
+
+# --- !Downs
+
+SET FOREIGN_KEY_CHECKS=0;
+
+SET FOREIGN_KEY_CHECKS=1;
+
